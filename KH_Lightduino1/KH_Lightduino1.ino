ae68fdb8f86522cb136d4223870c318fecc53ec4
twit-KnowHow <ashersimcha42@gmail.com>
2017-03-24 20:40:23 -0700

    Add files via upload
    Use potentiometers with an Arduino to control LED lights!
    We build an Arduino that can control, brightness, red, blue, and green LED's individually using potentiometers.
    
    Lightduino
    Parts List
    1. 4 x Potentiometers $6.98 http://www.ebay.com/itm/8Pcs-Single-3-Pin-Rotary-Potentiometer-Panel-Pot...
    2. Favorite WS2812 Light
    3. UBEC (For future use)
    
    Assembly
    1. Set 4 potentiometers into the breadboard
    2. Daisy chain 5v and GND onto the outside pins of each potentiometer (MAKE SURE NOT TO CROSS 5V and GND!)
    3. Connect the center pin of each potentiometer to Analog pins 0-3
    4. Connect the WS2812 light stick to 5V and GND, and the data pin to Digital Pin 3
    
    Practicals
    1. We're running everything from on-board power for this small-scale experiment, but we'll need a seperate power source once we scale up our lighting
    2. We're using an Uno for the prototyping stage, but we're going to use a smaller Atmel board for the final project
    3. We want to run the finished light from a sing

---
+++ KH_Lightduino1/KH_Lightduino1.ino
@@ -0,0 +1,98 @@
+
+#include "FastLED.h"
+#define DATA_PIN    3
+//#define CLK_PIN   4
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define NUM_LEDS    8
+#define FRAMES_PER_SECOND  120
+
+CRGB leds[NUM_LEDS];
+
+int BRIGHTNESS;
+
+
+void setup() {
+  delay(3000); // 3 second delay for recovery
+  
+  Serial.begin(9600);
+  
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+
+void loop()
+{
+
+  int RedPot = analogRead(0); // This checks the pot connected to Analog Pin 7 and gives us a value between 0-1024
+  int RedValue = map(RedPot, 70, 850, 0, 255); // This maps the value of the pot from 0-1020 to a value between 0-5
+  if (RedValue > 255)
+    RedValue = 255;
+  if (RedValue < 0)
+    RedValue = 0;
+    
+  int GreenPot = analogRead(1); // This checks the pot connected to Analog Pin 7 and gives us a value between 0-1024
+  int GreenValue = map(GreenPot, 70, 850, 0, 255); // This maps the value of the pot from 0-1020 to a value between 0-
+  if (GreenValue > 255)
+    GreenValue = 255;
+  if (GreenValue < 0)
+    GreenValue = 0;    
+
+  int BluePot = analogRead(2); // This checks the pot connected to Analog Pin 7 and gives us a value between 0-1024
+  int BlueValue = map(BluePot, 70, 850, 0, 255); // This maps the value of the pot from 0-1020 to a value between 0-5
+  if (BlueValue > 255)
+    BlueValue = 255;
+  if (BlueValue < 0)
+    BlueValue = 0;
+
+  BRIGHTNESS = analogRead(3); // This checks the pot connected to Analog Pin 7 and gives us a value between 0-1024
+  int BrightValue = map(BRIGHTNESS, 70, 850, 0, 255); // This maps the value of the pot from 0-1020 to a value between 0-5
+  if (BrightValue > 255)
+    BrightValue = 255;
+  if (BrightValue < 0)
+    BrightValue = 0;
+
+WriteDebug(RedPot, RedValue, GreenPot, GreenValue, BluePot, BlueValue);
+
+LiteUpArray(RedValue, GreenValue, BlueValue, BrightValue);
+
+//  leds[1].red = RedValue;
+//  leds[1].green = GreenValue;
+//  leds[1].blue = BlueValue;
+
+}
+
+
+void WriteDebug(int RedPot, int RedValue, int GreenPot, int GreenValue, int BluePot, int BlueValue)
+{
+  Serial.print(RedPot);
+  Serial.print("//");
+  Serial.print(RedValue); // debug -- Sends the value of the pot to the serial port
+  Serial.print("\t");  // debug -- Sends a tab
+  Serial.print(GreenPot);
+  Serial.print("//");
+  Serial.print(GreenValue); // debug -- Sends the value of the pot to the serial port
+  Serial.print("\t");  // debug -- Sends a tab
+  Serial.print(BluePot);
+  Serial.print("//");
+  Serial.print(BlueValue); // debug -- Sends the value of the pot to the serial port
+  Serial.print("\t");  // debug -- Sends a tab
+  Serial.print("\n");
+}
+
+void LiteUpArray(int RedValue, int GreenValue, int BlueValue, int BrightValue)
+{
+  for (int x = 0; x < NUM_LEDS; x++)
+    leds[x].setRGB (RedValue, GreenValue, BlueValue);
+   
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND);
+  FastLED.setBrightness(BrightValue);
+}
+
